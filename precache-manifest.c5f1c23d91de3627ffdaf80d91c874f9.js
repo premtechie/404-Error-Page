ea928a9d85de434baa5b5f85247c9e984d0aced6
premtechie <premtechie07@gmail.com>
2020-07-27 19:50:40 +0530

    Updates

---
+++ precache-manifest.c5f1c23d91de3627ffdaf80d91c874f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6aa15f8c79e6bfaa1cf355f96243ba",
+    "url": "/404-Error-Page/index.html"
+  },
+  {
+    "revision": "c1df4dcc7886ae8f143c",
+    "url": "/404-Error-Page/static/css/main.3be903ab.chunk.css"
+  },
+  {
+    "revision": "f228c7ce481e2c90544c",
+    "url": "/404-Error-Page/static/js/2.4d293b96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/404-Error-Page/static/js/2.4d293b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1df4dcc7886ae8f143c",
+    "url": "/404-Error-Page/static/js/main.ebebd4fe.chunk.js"
+  },
+  {
+    "revision": "48d33c6a9f28339ff79b",
+    "url": "/404-Error-Page/static/js/runtime-main.4caa9263.js"
+  },
+  {
+    "revision": "9f720a55ebc8ddf98c2593757aaa959c",
+    "url": "/404-Error-Page/static/media/Scarecrow.9f720a55.png"
+  }
+]);
